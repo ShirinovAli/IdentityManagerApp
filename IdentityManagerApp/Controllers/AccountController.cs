77ab3345b48b269b2c4cd815a983f1f47c6624c9
ShirinovAli <alinsh@code.edu.az>
2024-02-16 23:27:10 +0400

    Two Factor Auth added

--- IdentityManagerApp/Controllers/AccountController.cs
+++ IdentityManagerApp/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using IdentityManagerApp.Models;
 using IdentityManagerApp.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -45,6 +46,19 @@ namespace IdentityManagerApp.Controllers
                 AddErrors(result);
                 return View(model);
             }
+
+            var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            var callbackUrl = Url.Action("ConfirmEmail", "Account", new
+            {
+                userId = user.Id,
+                code
+            }, protocol: HttpContext.Request.Scheme);
+
+            await _emailSender
+                    .SendEmailAsync(model.Email,
+                                    "Confirm Email - Identity Manager App",
+                                    $"Please confirm your email by clicking here: <a href='{callbackUrl}'>link</a>");
+
             await _signInManager.SignInAsync(user, isPersistent: false);
             return RedirectToAction("Index", "Home");
         }
@@ -80,6 +94,9 @@ namespace IdentityManagerApp.Controllers
                 if (result.IsLockedOut)
                     return View("Lockout");
 
+                if (result.RequiresTwoFactor)
+                    return RedirectToAction(nameof(VerifyAuthenticatorCode), new { returnurl, model.RememberMe });
+
                 ModelState.AddModelError(string.Empty, "Invalid login attempt.");
                 return View(model);
             }
@@ -91,6 +108,23 @@ namespace IdentityManagerApp.Controllers
         public IActionResult Lockout()
         {
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ConfirmEmail(string code, string userId)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByIdAsync(userId);
+                if (user == null)
+                    return View("Error");
+
+                var result = await _userManager.ConfirmEmailAsync(user, code);
+                if (result.Succeeded)
+                    return View();
+
+            }
+            return View("Error");
         }
 
         [HttpGet]
@@ -125,7 +159,7 @@ namespace IdentityManagerApp.Controllers
                      "Reset Password - Identity Manager App",
                      $"Please reset your password by clicking here: <a href='{callbackUrl}'>link</a>");
 
-            return RedirectToAction("ForgotPasswordConfirmation");
+            return RedirectToAction(nameof(ForgotPasswordConfirmation));
         }
 
         [HttpGet]
@@ -136,8 +170,20 @@ namespace IdentityManagerApp.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult ResetPassword(ResetPasswordModel model)
+        public async Task<IActionResult> ResetPassword(ResetPasswordModel model)
         {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var user = await _userManager.FindByEmailAsync(model.Email);
+            if (user == null)
+                return RedirectToAction(nameof(ResetPasswordConfirmation));
+
+            var result = await _userManager.ResetPasswordAsync(user, model.Code, model.Password);
+            if (result.Succeeded)
+                return RedirectToAction(nameof(ResetPasswordConfirmation));
+
+            AddErrors(result);
             return View();
         }
 
@@ -149,6 +195,81 @@ namespace IdentityManagerApp.Controllers
 
         [HttpGet]
         public IActionResult ForgotPasswordConfirmation()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AuthenticatorConfirmation()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> EnableAuthenticator()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            await _userManager.ResetAuthenticatorKeyAsync(user);
+            var token = await _userManager.GetAuthenticatorKeyAsync(user);
+            TwoFactorAuthenticationModel model = new() { Token = token };
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EnableAuthenticator(TwoFactorAuthenticationModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.GetUserAsync(User);
+                var succeeded = await _userManager.VerifyTwoFactorTokenAsync(user, _userManager.Options.Tokens.AuthenticatorTokenProvider, model.Code);
+                if (succeeded)
+                {
+                    await _userManager.SetTwoFactorEnabledAsync(user, true);
+                }
+                else
+                {
+                    ModelState.AddModelError("Verify", "Your two factor auth code could not be validated.");
+                    return View(model);
+                }
+                return RedirectToAction(nameof(AuthenticatorConfirmation));
+            }
+            return View("Error");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> VerifyAuthenticatorCode(bool rememberMe, string returnurl = null)
+        {
+            var user = await _signInManager.GetTwoFactorAuthenticationUserAsync();
+            if (user == null)
+                return View(nameof(Error));
+
+            ViewData["ReturnUrl"] = returnurl;
+            return View(new VerifyAuthenticatorModel { ReturnUrl = returnurl, RememberMe = rememberMe });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> VerifyAuthenticatorCode(VerifyAuthenticatorModel model)
+        {
+            model.ReturnUrl = model.ReturnUrl ?? Url.Content("~/");
+
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var result = await _signInManager.TwoFactorAuthenticatorSignInAsync(model.Code, model.RememberMe, rememberClient: false); ;
+            if (result.Succeeded)
+                return LocalRedirect(model.ReturnUrl);
+            if (result.IsLockedOut)
+                return View("Lockout");
+
+            ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Error()
         {
             return View();
         }
---
+++ IdentityManagerApp/Models/ViewModels/TwoFactorAuthenticationModel.cs
@@ -0,0 +1,8 @@
+﻿namespace IdentityManagerApp.Models.ViewModels
+{
+    public class TwoFactorAuthenticationModel
+    {
+        public string Code { get; set; }
+        public string? Token { get; set; }
+    }
+}
